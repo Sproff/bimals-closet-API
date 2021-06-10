ce9a2dfa31940c10f3d202caed80689d9c433fe5
Sproff <sproff.oluwaseun@gmail.com>
2021-06-10 13:09:20 +0100

    added the search integration

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 const routeRouter = require('./routes/route');
 const userRoute = require('./routes/user.route');
-const {handleError} = require('./utils/errorHandler');
+const { handleError } = require('./utils/errorHandler');
 const port = process.env.PORT || 9000;
 
 app.use(express.json());
@@ -16,15 +16,15 @@ app.use('/', routeRouter);
 app.use('/user', userRoute);
 app.use(handleError);
 
-  async function init() {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-  
-    app.listen(port, () => {
-      console.log(`Server running on port: ${port}`)
-    })
-    console.log('DB Connected Successfully');
-  }
-  init()
+async function init() {
+  await mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+  });
+  console.log('DB Connected Successfully');
+}
+init();
