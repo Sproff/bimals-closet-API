d9efc7d6ab6c5257412ddde3f2bcb4b395afcf68
Sproff <sproff.oluwaseun@gmail.com>
2021-06-15 01:09:08 +0100

    changed error convention

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -115,7 +115,7 @@ const searchProduct = async (req, res, next) => {
     const { q } = req.query;
     const products = await Product.find({ name: { $regex: q, $options: 'i' } });
 
-    if (products.length < 1) throw new ErrorHandler(404, 'No Product found');
+    if (products.length < 1) throw new ErrorHandler(404, 'No product found');
 
     res.status(201).json({
       status: 'success',
